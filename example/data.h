65c61d32d5272ad9b99417260d33f15b50ea5a8d
Shreejit Murthy <botmorph17@gmail.com>
2024-12-17 14:40:17 +1030

    create examples

---
+++ example/data.h
@@ -0,0 +1,15 @@
+/* Auto-generated code - do not edit! */
+
+typedef struct PlayerData {
+   int health;
+   int level;
+} PlayerData;
+
+typedef struct EnemyData {
+   int health;
+} EnemyData;
+
+typedef struct AllyData {
+   int friendship;
+} AllyData;
+
